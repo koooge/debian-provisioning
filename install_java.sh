43093b06484f8334897219903fd06ec4c438cbfa
koooge <koooooge@gmail.com>
2016-12-18 11:18:30 +0100

    * install_java.sh: test ok

--- install_java.sh
+++ install_java.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-# https://www.linkedin.com/pulse/installing-openjdk-8-tomcat-debian-jessie-iga-made-muliarsa
+# cf. https://www.linkedin.com/pulse/installing-openjdk-8-tomcat-debian-jessie-iga-made-muliarsa
 
 # java
-sudo sh -c "echo 'deb http://ftp.de.debian.org/debian jessie-backports main' >> /etc/apt/sources.list.d/java.list"
-sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk
+install_java() {
+  sudo sh -c "echo 'deb http://ftp.de.debian.org/debian jessie-backports main' >> /etc/apt/sources.list.d/java.list"
+  sudo apt-get update
+  sudo apt-get install -y openjdk-8-jdk
+}
+
+install_java
 
 java -version
