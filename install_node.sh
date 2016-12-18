826c5269fbbffe2e976070cc8d820b5f72842a53
koooge <koooooge@gmail.com>
2016-12-18 11:12:44 +0100

    * install_node.sh: test ok

--- install_node.sh
+++ install_node.sh
@@ -1,16 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-## nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-echo '' >> ~/.bash_profile
-echo '# node' >> ~/.bash_profile
-echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_profile
-echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.bash_profile
-source ~/.bash_profile
+shopt -s expand_aliases
 
-## node
-nvm install lts/boron
-# nvm install v6.9.1
+install_nvm() {
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+  echo '' >> ~/.bash_profile
+  echo '# node' >> ~/.bash_profile
+  echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_profile
+  echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.bash_profile
+  source ~/.bash_profile
+}
+
+install_node() {
+  nvm install lts/boron
+  nvm use lts/boron
+}
+
+install_nvm
+install_node
 
 nvm version
 node -v
