1301d5bcd84ac6e68aee527da4b5392e4d87445f
koooge <koooooge@gmail.com>
2016-12-18 10:29:40 +0100

    * install_docker.sh: test ok

--- install_docker.sh
+++ install_docker.sh
@@ -1,23 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 ## docker
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo sh -c "echo 'deb https://apt.dockerproject.org/repo debian-jessie main' >> /etc/apt/sources.list.d/docker.list"
-sudo apt-get update
-apt-cache policy docker-engine
-sudo apt-get install -y docker-engine
-sudo service docker start
-### sudo groupadd docker
-sudo gpasswd -a ${USER} docker
-sudo service docker restart
+install_docker() {
+  apt-get update
+  apt-get install -y apt-transport-https ca-certificates
+  apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+  echo 'deb https://apt.dockerproject.org/repo debian-jessie main' >> /etc/apt/sources.list.d/docker.list
+  apt-get update
+  apt-cache policy docker-engine
+  apt-get install -y docker-engine
+  service docker start
+
+  ### sudo groupadd docker
+  gpasswd -a ${USER} docker
+  service docker restart
+}
 
 ## dockere-compose
-sudo -i
-curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
-exit
-sudo chmod +x /usr/local/bin/docker-compose
+install_docker_compose() {
+  curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+}
+
+install_docker
+install_docker_compose
 
 docker -v
 docker-compose -v
