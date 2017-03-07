b70306689e61d14cb5109747e8ea340634a80137
koooge <koooooge@gmail.com>
2017-03-07 00:32:22 +0000

    add dotfiles

--- debian.sh
+++ debian.sh
@@ -2,10 +2,13 @@
 
 # Usage: `sudo ./debian.h`
 
-# packages
+## packages
 apt-get update
 apt-get -y upgrade
 apt-get install -y git tmux curl tree
+
+## dotfiles
+curl -L raw.github.com/koooge/dotfiles/master/install.sh | bash
 
 ## alias
 alias ll='ls -l'
