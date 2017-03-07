c66fb6ea0c148d853d5e84ef04d5465b87760387
koooge <koooooge@gmail.com>
2017-03-07 01:51:45 +0000

    create embulk installer

---
+++ install_embulk.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Usage: `./install_embulk.sh`
+
+curl --create-dirs -o ~/.embulk/bin/embulk -L "https://dl.embulk.org/embulk-latest.jar"
+chmod +x ~/.embulk/bin/embulk
+echo 'export PATH="$HOME/.embulk/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
