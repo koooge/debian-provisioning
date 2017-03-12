16a120a88a87bdb06625e3a8b7deb46c1eb4db57
koooge <koooooge@gmail.com>
2017-03-12 11:11:40 +0100

    use sdkman

--- install_java.sh
+++ install_java.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-# Usage: `sudo ./install_java.sh`
+# Usage: `./install_java.sh`
 
-install_java() {
-  echo "deb http://ftp.jp.debian.org/debian/ jessie-backports main" >> /etc/apt/sources.list
-  apt-get update
-  apt install -y -t jessie-backports  openjdk-8-jre-headless ca-certificates-java
-  apt-get install -y openjdk-8-jdk
+install_sdkman() {
+  curl -s "https://get.sdkman.io" | bash
+  source "$HOME/.sdkman/bin/sdkman-init.sh"
 }
 
-install_java
+install_java() {
+  sdk install java
+}
 
+install_maven() {
+  sdk install maven
+}
+
+install_sdkman
+install_java
+install_maven
+
+sdk version
 java -version
+mvn -v
