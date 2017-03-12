b34857eb6665ad24e6179ab74487785c2316fd40
vagrant <vagrant@debian>
2017-03-12 10:39:02 +0100

    add unzip

--- debian.sh
+++ debian.sh
@@ -5,7 +5,7 @@
 ## packages
 apt-get update
 apt-get -y upgrade
-apt-get install -y git tmux curl \
+apt-get install -y git tmux curl unzip \
     tree jq
 
 ## dotfiles
@@ -15,8 +15,3 @@ curl -L raw.github.com/koooge/dotfiles/master/install.sh | bash
 echo "" >> ~/.bash_profile
 echo "## aliases" >> ~/.bash_profile
 echo "alias ll='ls -l'" >> ~/.bash_profile
-
-## docker
-## node
-## ruby
-## java
