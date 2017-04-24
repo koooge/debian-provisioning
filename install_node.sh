81aba2c3dc6d2cc6bd4500c0a618f0673869d233
koooge <koge.takuma@jp.fujitsu.com>
2017-04-24 13:56:27 +0900

    bumpup nvm

--- install_node.sh
+++ install_node.sh
@@ -5,7 +5,7 @@
 shopt -s expand_aliases
 
 install_nvm() {
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
   cp ./.bash.d/node.sh ${HOME}/.bash.d/
   source ${HOME}/.bash.d/node.sh
 }
