753fdb848109144b60e5735b72e5a7e9408df2ec
koooge <koge.takuma@jp.fujitsu.com>
2017-04-24 13:52:49 +0900

    * .bash.d/embulk.sh: move embulk settings from .bash_profile * install_embulk: ditto

--- install_embulk.sh
+++ install_embulk.sh
@@ -4,4 +4,3 @@
 
 curl --create-dirs -o ~/.embulk/bin/embulk -L "https://dl.embulk.org/embulk-latest.jar"
 chmod +x ~/.embulk/bin/embulk
-echo 'export PATH="$HOME/.embulk/bin:$PATH"' >> ~/.bash_profile
