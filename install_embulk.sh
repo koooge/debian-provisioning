de85d9df3add1f4629fbbdeb9a6a6bc6773f1b2d
koooge <koge.takuma@jp.fujitsu.com>
2017-04-24 13:53:50 +0900

    chmod +x and change Usage

--- install_embulk.sh
+++ install_embulk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage: `source install_embulk.sh`
+# Usage: `./install_embulk.sh`
 
 curl --create-dirs -o ~/.embulk/bin/embulk -L "https://dl.embulk.org/embulk-latest.jar"
 chmod +x ~/.embulk/bin/embulk
