6e75e4fbe065aa18741e55a6c54e80ba84926ff3
koooge <koooooge@gmail.com>
2017-06-26 19:38:22 +0200

    set version by var

--- install_node.sh
+++ install_node.sh
@@ -4,15 +4,18 @@
 
 shopt -s expand_aliases
 
+NVM_VERSION="v0.33.2"
+NODE_VERSION="lts/boron"
+
 install_nvm() {
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh | bash
   cp ./.bash.d/node.sh ${HOME}/.bash.d/
   source ${HOME}/.bash.d/node.sh
 }
 
 install_node() {
-  nvm install lts/boron
-  nvm use lts/boron
+  nvm install ${NODE_VERSION}
+  nvm use ${NODE_VERSION}
 }
 
 install_nvm
