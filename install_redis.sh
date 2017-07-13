f735284f1216598b6de15db6bfa2dc600115f59c
koooge <koooooge@gmail.com>
2017-07-13 07:50:58 +0200

    redis installer

---
+++ install_redis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Usage: `./install_redis.sh`
+
+REDIS_VERSION=3.2.9
+
+wget http://download.redis.io/releases/redis-${REDIS_VERSION}.tar.gz
+tar xzf redis-${REDIS_VERSION}.tar.gz
+cd redis-${REDIS_VERSION}
+make
+
+cd -
+rm redis-${REDIS_VERSION}.tar.gz
